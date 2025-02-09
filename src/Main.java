ffe6189d53bc9868922daa17ae51361ecc240afd
Haydenj213 <johns3hm@mail.uc.edu>
2025-02-09 16:28:21 -0500

    Final Code Added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Please enter your party affiliation.\n D for Democratic.\n R for Republican.\n I for independent.\n Anything else for other.");
+
+        String partyAffiliation = in.nextLine();
+
+        if (Objects.equals(partyAffiliation, "D")) {
+            System.out.println("You get a Democratic Donkey");
+        } else if (Objects.equals(partyAffiliation, "R")) {
+            System.out.println("You get a Republican Elephant");
+        } else if (Objects.equals(partyAffiliation, "I")) {
+            System.out.println("You get an Independent Man");
+        } else {
+            System.out.println("Thank you for your alternative answer. It will be recorded, so that we may review it and potentially add it to our next questioner.");
+        }
+
     }
 }
